f13be2c6f57727f84da18c0a686f8a36237d2625
Victor Bredarioli <victorcbr@gmail.com>
2020-05-29 20:48:12 -0300

    Page Home - style update

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,18 +1,30 @@
 import styled from "styled-components";
-import ImgBackground from "theme/images/background.jpg";
-import { TextField, Button } from "@material-ui/core";
 
-export const Container = styled.div`
+import { Container as ContainerTheme } from "theme/styles";
+
+export const Container = styled(ContainerTheme)`
+  flex-direction: row;
+  /* TODO */
+  /* @media screen {
+    flex-direction: column;
+  } */
+`;
+
+export const Menu = styled.div`
   display: flex;
+  flex: 1;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 100%;
   height: 100vh;
+  padding: 0 10px;
+  border-right: 1px solid rgba(169, 158, 126, 0.1);
+`;
 
-  background: url(${ImgBackground}) no-repeat center center fixed;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
+export const Content = styled.div`
+  display: flex;
+  flex: 3;
+  flex-direction: column;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.3);
 `;

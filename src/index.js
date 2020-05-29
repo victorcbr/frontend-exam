bb88f5e0c2dbeba2eace90480755a55f09dc88c9
Victor Bredarioli <victorcbr@gmail.com>
2020-05-29 03:47:17 -0300

    config react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "theme/css/index.css";
-import LoginPage from "pages/Login";
+import { App } from "pages";
 import * as serviceWorker from "serviceWorker";
 import { AuthController } from "context/AuthContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthController>
-      <LoginPage />
+      <App />
     </AuthController>
   </React.StrictMode>,
   document.getElementById("root")

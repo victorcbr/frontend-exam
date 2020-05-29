2f69c11fc8d5bc0d981718f31ecb32ab71848eac
Victor Bredarioli <victorcbr@gmail.com>
2020-05-28 21:42:44 -0300

    Button Component

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+const ButtonComponent = (props) => {
+  return <Button {...props} />;
+};
+
+export default ButtonComponent;

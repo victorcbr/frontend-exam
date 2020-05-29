016ac2199fcdb862a4fe42f886c6441b6af0e94d
Victor Bredarioli <victorcbr@gmail.com>
2020-05-28 21:43:42 -0300

    Login Page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,27 @@
 import React from "react";
 
 import Logo from "components/Logo";
+import Input from "components/Input";
+
+import { Container, Form, CustomButton } from "./styles";
+
+const FormAuth = () => {
+  return (
+    <Form noValidate autoComplete="off">
+      <Input label="Email" />
+      <Input label="Password" style={{ marginBottom: 20 }} />
+      <CustomButton variant="outlined">Login</CustomButton>
+    </Form>
+  );
+};
 
 const LoginPage = () => {
-  return <Logo />;
+  return (
+    <Container>
+      <Logo />
+      <FormAuth />
+    </Container>
+  );
 };
 
 export default LoginPage;

e3c6fc74858be772593615c75a206228daf7c321
Victor Bredarioli <victorcbr@gmail.com>
2020-05-31 04:56:34 -0300

    Page Home - Integration Youtube Api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,69 @@
-import React, { useContext, useEffect } from "react";
+/* eslint-disable react/prop-types */
+import React, { useState, useContext, useEffect, useCallback } from "react";
 
 import { AuthContext } from "context/AuthContext";
 
-import { Container, Menu, Content } from "./styles";
+import {
+  Container,
+  Menu,
+  Content,
+  VideoLink,
+  VideoContent,
+  VideoTitle,
+} from "./styles";
 import { CustomButton } from "theme/styles";
 
 import Logo from "components/Logo";
 
+import { getVideosByPlaylist } from "config/api";
+
 const Home = ({ history }) => {
   const [authState, updateAuth] = useContext(AuthContext);
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
     if (!authState.loggedIn) return history.push("/login");
-  }, [authState.loggedIn, history]);
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    _getVideos();
+    // eslint-disable-next-line
+  }, []);
+
+  const _getVideos = useCallback(async () => {
+    return await getVideosByPlaylist().then(({ items }) => {
+      return setVideos(items);
+      // return
+    });
+  }, []);
 
   const _signOut = async () => {
     return await updateAuth({
       ...authState,
       loggedIn: false,
       user: {},
+    });
+  };
+
+  const VideosList = () => {
+    return videos.map((item) => {
+      return (
+        <VideoLink
+          href={`https://www.youtube.com/watch?v=${item.snippet.resourceId.videoId}&list=${item.snippet.playlistId}`}
+          target="_blank"
+          rel="noreferrer"
+          key={item.snippet.resourceId.videoId}
+        >
+          <VideoContent
+            width={item.snippet.thumbnails.high.width}
+            height={item.snippet.thumbnails.high.height}
+            backgroundUrl={item.snippet.thumbnails.high.url}
+          >
+            <VideoTitle>{item.snippet.title}</VideoTitle>
+          </VideoContent>
+        </VideoLink>
+      );
     });
   };
 
@@ -36,7 +81,9 @@ const Home = ({ history }) => {
         </div>
       </Menu>
 
-      <Content />
+      <Content>
+        <VideosList />
+      </Content>
     </Container>
   );
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -24,7 +24,41 @@ export const Menu = styled.div`
 export const Content = styled.div`
   display: flex;
   flex: 3;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  overflow: scroll;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.3);
+`;
+
+export const VideoLink = styled.a`
+  margin: 20px 15px;
+  border: 1px solid #a99e7e;
+  display: flex;
+  justify-content: center;
+  text-decoration: none;
+`;
+export const VideoContent = styled.div`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  background: url(${(props) => props.backgroundUrl}) no-repeat center center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+export const VideoTitle = styled.p`
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  width: 100%;
+  padding: 15px;
+  background-color: rgba(0, 0, 0, 0.6);
 `;
